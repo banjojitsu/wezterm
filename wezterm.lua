42ad2c60d74a928f72d5eb3c8d3b61fd23f02332
jason <105950172+banjojitsu@users.noreply.github.com>
2024-10-26 12:16:36 -0400

    update configurations

--- wezterm.lua
+++ wezterm.lua
@@ -2,5 +2,15 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.color_scheme = 'AdventureTime'
+config.window_close_confirmation = 'NeverPrompt'
+config.window_decorations = 'RESIZE'
+config.enable_scroll_bar = false
+config.enable_tab_bar = false
+config.window_padding = {
+  left = 35,
+  right = 35,
+  top = 35,
+  bottom = 35,
+}
 
 return config
