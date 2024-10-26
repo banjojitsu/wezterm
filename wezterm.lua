5ef57588864ee109492f0328a90bc961c1091910
jason <105950172+banjojitsu@users.noreply.github.com>
2024-10-26 14:07:37 -0400

    update color scheme

--- wezterm.lua
+++ wezterm.lua
@@ -1,14 +1,14 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'AdventureTime'
+config.color_scheme = 'Tokyo Night Moon'
 config.window_close_confirmation = 'NeverPrompt'
 config.window_decorations = 'RESIZE'
 config.enable_scroll_bar = false
 config.enable_tab_bar = false
 config.window_padding = {
-  left = 35,
-  right = 35,
+  left = 30,
+  right = 30,
   top = 35,
   bottom = 35,
 }
